b7f0425243da19ba3b14e489e244da878f9d351d
BlueCut <bluecut.official@gmail.com>
2017-09-11 18:34:38 +0200

    Sort films : implement the select item

--- app/controllers/films.js
+++ app/controllers/films.js
@@ -7,26 +7,39 @@ const {
 
 export default Controller.extend({
 
-  queryParams: ['sort'],
-  // Recent first by default
-  sort: 'releaseDateDesc',
-
+  // Available options for queryParams : key=sort, value=parameter for sortProperty
   sortOptions: {
     'titleAsc': ['title:asc'],
     'releaseDateDesc': ['release_date:desc'],
     'releaseDateAsc': ['release_date:asc']
   },
+  // Match between select values and available sort values
+  match: {
+    'Alphabetic': 'titleAsc',
+    'Recent first': 'releaseDateDesc',
+    'Old first': 'releaseDateAsc'
+  },
 
+  queryParams: ['sort'],
+  sort: 'releaseDateDesc',
+
+  // Select item
+  selectableOptions: ['Recent first', 'Old first', 'Alphabetic'],
+  selectedOption: '',
+
+  // The array used as parameter for sortedFilms
   sortProperty: computed('sort', 'sortOptions', function() {
     return this.get('sortOptions')[this.get('sort')];
   }),
 
-  sortedFilms: computed.sort('model', 'sortProperty')
+  // Sorted model
+  sortedFilms: computed.sort('model', 'sortProperty'),
 
-  // actions: {
-  //   setSorting(option) {
-  //     this.set('sortBy', option);
-  //   }
-  // }
+  actions: {
+    setSorting(option) {
+      this.set('selectedOption', option);
+      this.set('sort', this.get('match')[option]);
+    }
+  }
 
 });
